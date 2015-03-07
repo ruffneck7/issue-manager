7e919c413b0aa2a95cb544210eb8976da90652fb
ruffneck7 <ruffneck7@gmail.com>
2015-03-07 12:54:05 +0900

    support for mobile

--- src/test/java/learningspringboot/IssueManagerApplicationTests.java
+++ src/test/java/learningspringboot/IssueManagerApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = IssueManagerApplication.class)
 @WebAppConfiguration
-public class    IssueManagerApplicationTests {
+public class IssueManagerApplicationTests {
 
 	@Test
 	public void contextLoads() {
